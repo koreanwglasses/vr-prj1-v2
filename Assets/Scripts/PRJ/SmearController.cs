4c87bd8e93a4660ad6541ead3494cfda7831cf36
koreanwglasses <koreanwglasses@gmail.com>
2020-02-08 13:32:18 -0500

    Added dynamic sculpture

--- Assets/Scripts/PRJ/SmearController.cs
+++ Assets/Scripts/PRJ/SmearController.cs
@@ -8,14 +8,19 @@ public class SmearController : MonoBehaviour
     public GameObject control;
 
     [Header("Parameters")]
-    public Vector3 baseScale = new Vector3(0, 0, 0);
     public Vector3 scaleInfluence = new Vector3(1,1,1);
+    public bool rotate = true;
 
+    [Header("Additional Options")]
+    public Matrix4x4 inputTransform = Matrix4x4.identity;
+
+    private Vector3 baseScale;
     private MovementEstimator movementEstimator;
 
     // Start is called before the first frame update
     void Start()
     {
+        baseScale = this.gameObject.transform.localScale;
         movementEstimator = control.GetComponent<MovementEstimator>();    
     }
 
@@ -34,12 +39,27 @@ public class SmearController : MonoBehaviour
         return new Vector3(res_x, res_y, res_z);
     }
 
+    private Vector3 DualScAddV(Vector3 x, Vector3 y, Vector3 z)
+    {
+        float res_x = DualScAdd(x.x, y.x, z.x);
+        float res_y = DualScAdd(x.y, y.y, z.y);
+        float res_z = DualScAdd(x.z, y.z, z.z);
+        return new Vector3(res_x, res_y, res_z);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 v = movementEstimator.linearVelocity;
+        Vector3 v = inputTransform * movementEstimator.linearVelocity;
 
-        this.gameObject.transform.LookAt(this.gameObject.transform.position + v);
-        this.gameObject.transform.localScale = DualScAddV(baseScale, v.sqrMagnitude, scaleInfluence);
+        if (rotate)
+        {
+            this.gameObject.transform.localScale = DualScAddV(baseScale, v.sqrMagnitude, scaleInfluence);
+            this.gameObject.transform.LookAt(this.gameObject.transform.position + v);
+        }
+        else
+        {
+            this.gameObject.transform.localScale = DualScAddV(baseScale, Vector3.Scale(v, v), scaleInfluence);
+        }
     }
 }
