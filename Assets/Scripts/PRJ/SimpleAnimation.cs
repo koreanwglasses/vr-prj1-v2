8e8baf263819bb6c3490db4954514b91a195fb99
Fred C <koreanwglasses@gmail.com>
2020-02-10 13:50:29 -0500

    lights

---
+++ Assets/Scripts/PRJ/SimpleAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimation : MonoBehaviour
+{
+    public Vector3 angularVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion localRotation = this.gameObject.transform.rotation;
+        localRotation.ToAngleAxis(out float angle, out Vector3 axis);
+        Vector3 newRotation = axis * angle + angularVelocity * Time.deltaTime;
+        this.gameObject.transform.rotation = Quaternion.AngleAxis(newRotation.magnitude, newRotation.normalized);
+    }
+}
---
+++ Assets/Scripts/PRJ/SimpleNavigation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleNavigation : MonoBehaviour
+{
+
+    public float threshold = 0.1f;
+    public float acceleration = 1f;
+    public float maxSpeed = 10f;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(h, 0, v);
+
+        if(moveVector.magnitude > threshold)
+        {
+            velocity += (moveVector * acceleration) * Time.deltaTime;
+        } else
+        {
+            Vector3 newVelocity = velocity + (-velocity * acceleration) * Time.deltaTime;
+            if(Vector3.Dot(velocity, newVelocity) < 0)
+            {
+                velocity = new Vector3(0, 0, 0);
+            } else
+            {
+                velocity = newVelocity;
+            }
+        }
+        if(velocity.magnitude > maxSpeed)
+        {
+            velocity = velocity.normalized * maxSpeed;
+        }
+        this.gameObject.transform.position += velocity * Time.deltaTime;
+    }
+}
